5558c36e68e5a3eee5911c1997428a20cd2cabff
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-22 01:31:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Olá usuário, vou ajudá-lo a coverter °C para °F. Digite uma tecla para continuar:");
+
+            Console.ReadKey();
+
+
+
+            Console.WriteLine("Digite o valor em celcius:");
+
+            double celcius = double.Parse (Console.ReadLine ());
+
+
+
+            double valor = 1.8;
+
+            double farenheit  = 32;
+
+
+
+            Console.Write(celcius + " graus celcius equivalem a: ");
+
+            Console.Write(celcius * valor + farenheit );
+
+            Console.WriteLine(" graus farenheit.");
+
+
+        }
+    }
+}
